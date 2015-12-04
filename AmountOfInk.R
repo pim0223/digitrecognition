80a1cabccba3f676f1016142067b979523f7d892
Jesse <j.h.de@Hotmail.com>
2015-12-04 12:53:57 +0100

    Multinom
    In amountofink.r staan 2 functies voor multinom berekenen. In de
    mnist_density.rda staat mnist met amount of ink berekend en scaled. In
    multinomimage.rdata staat de session met multinoms etc.

--- AmountOfInk.R
+++ AmountOfInk.R
@@ -3,10 +3,11 @@ densityFunc <- function(table){
   
 amountOfInk <- vector()
 i = 0
-for(row in 1:42000)
+for(row in 1:nrow(table))
 {
   i = i + 1
   ink = 0
+  #misschien 2:ncol of params?
   for(col in 2:785)
   {
     ink = ink + as.numeric(table[row,col])
@@ -16,4 +17,16 @@ for(row in 1:42000)
 }
 
 return(amountOfInk)
+}
+
+#make a multinom of scaled density to label given data.
+makeMultinom <- function(data){
+  data.multinom <- multinom(label ~ scaledDensity, data)
+  return(data.multinom)
+}
+
+#make a prediction of data given a multinom function and data.
+predict <- function(multinom, data){
+  data.pred <- predict(multinom, data[,-c(1)])
+  return(data.pred)
 }
